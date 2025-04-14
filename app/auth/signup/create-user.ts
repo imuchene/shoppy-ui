5c09ba425b0da93e88f208f6b83ebe17bd9427ab
Isaiah Muchene <imuchene@msn.com>
2025-04-14 17:19:55 +0300

    Created the server actions for signup

---
+++ app/auth/signup/create-user.ts
@@ -0,0 +1,18 @@
+"use server";
+
+import { API_URL } from "@/app/environments/environment";
+import { getErrorMessage } from "@/app/util/errors";
+import { redirect } from "next/navigation";
+
+
+export default async function createUser(_prevState: any, formData: FormData) {
+  const res = await fetch(`${API_URL}/users`, { method: "POST", body: formData});
+
+  const parsedRes = await res.json();
+
+  if (!res.ok) {
+    return { error: getErrorMessage(parsedRes) };
+  }
+
+  redirect("/");
+}

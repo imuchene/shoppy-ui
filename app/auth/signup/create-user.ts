eec8fa9053a143dada0754be8052160e1c383ed0
Isaiah Muchene <imuchene@msn.com>
2025-04-22 16:58:45 +0300

    Created the UI logic for login

--- app/auth/signup/create-user.ts
+++ app/auth/signup/create-user.ts
@@ -1,9 +1,13 @@
 'use server';
 
+import { FormError } from '@/app/common/interfaces/form-error.interface';
 import { post } from '@/app/util/fetch';
 import { redirect } from 'next/navigation';
 
-export default async function createUser(_prevState: any, formData: FormData) {
+export default async function createUser(
+  _prevState: FormError,
+  formData: FormData,
+) {
   const { error } = await post('users', formData);
   if (error) {
     return { error };

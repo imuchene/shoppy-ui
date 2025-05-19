2a3193ec89fa8cfd70ed8e9df4b46f8617e69ac0
Isaiah Muchene <imuchene@msn.com>
2025-05-19 17:00:07 +0300

    Added the product image to the product layout

--- app/products/product.tsx
+++ app/products/product.tsx
@@ -1,5 +1,7 @@
-import { Card, Typography } from '@mui/material';
+import { Card, Stack, Typography } from '@mui/material';
 import { Product as IProduct } from './interface/product.interface';
+import Image from 'next/image';
+import { API_URL } from '../common/constants/environment';
 
 interface ProductProps {
   product: IProduct;
@@ -8,9 +10,21 @@ interface ProductProps {
 export default function Product({ product }: ProductProps) {
   return (
     <Card className='p-4'>
-      <Typography variant='h4'>{product.name}</Typography>
-      <Typography>{product.description}</Typography>
-      <Typography>${product.price}</Typography>
+      <Stack gap={3}>
+        <Typography variant='h4'>{product.name}</Typography>
+        {product.imageExists && (
+          <Image
+            src={`${API_URL}/products/${product.id}.jpeg`}
+            width='0'
+            height='0'
+            className='h-auto w-full'
+            alt='Picture of the Product'
+            sizes='100vw'
+          />
+        )}
+        <Typography>{product.description}</Typography>
+        <Typography>${product.price}</Typography>
+      </Stack>
     </Card>
   );
 }
